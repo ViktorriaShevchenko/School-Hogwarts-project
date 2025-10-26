7e500ea096ed6c5b415d0029cb0f0cfdeb862fdf
Viktoria <vs118475@gmail.com>
2025-10-26 19:23:43 +0300

    A ManyToOne relationship between students and departments has been configured using JPA. Endpoints have been added for retrieving a student's department and the students of a department. Student filtering by age range and searching for departments by name/color have been implemented. A scripts.sql file with SQL queries has been created. Cyclic reference issues during JSON serialization have been fixed. All CRUD operations are processed via the REST API and stored in PostgreSQL.

--- src/main/java/ru/hogwarts/school/repository/StudentRepository.java
+++ src/main/java/ru/hogwarts/school/repository/StudentRepository.java
@@ -7,4 +7,8 @@ import java.util.Collection;
 
 public interface StudentRepository extends JpaRepository <Student, Long> {
     Collection<Student> findByAge(int age);
+
+    Collection<Student> findByAgeBetween(int minAge, int maxAge);
+
+    Collection<Student> findByFacultyId(Long facultyId);
 }
