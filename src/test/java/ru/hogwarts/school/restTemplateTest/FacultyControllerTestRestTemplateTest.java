3ef5f23d57d67a5354b063bc5b1d987f3f8ced2d
Viktoria <vs118475@gmail.com>
2025-11-21 21:45:35 +0300

    Fix test structure: separate WebMvcTest and TestRestTemplate tests

---
+++ src/test/java/ru/hogwarts/school/restTemplateTest/FacultyControllerTestRestTemplateTest.java
@@ -0,0 +1,118 @@
+package ru.hogwarts.school.restTemplateTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.ResponseEntity;
+import ru.hogwarts.school.controller.FacultyController;
+import ru.hogwarts.school.model.Faculty;
+
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class FacultyControllerTestRestTemplateTest {
+
+    private static final String BASE_URL = "http://localhost:";
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Autowired
+    private FacultyController facultyController;
+
+    @Test
+    void contextLoads() {
+        assertNotNull(facultyController);
+    }
+
+    @Test
+    void getFacultyTest() {
+        ResponseEntity<Faculty> response = restTemplate.getForEntity(
+                BASE_URL + port + "/faculty/1",
+                Faculty.class
+        );
+        assertNotNull(response.getBody());
+    }
+
+    @Test
+    void getFacultyNotFoundTest() {
+        ResponseEntity<Faculty> response = restTemplate.getForEntity(
+                BASE_URL + port + "/faculty/999",
+                Faculty.class
+        );
+        assertNotEquals(200, response.getStatusCodeValue());
+    }
+
+    @Test
+    void createFacultyTest() {
+        Faculty faculty = new Faculty();
+        faculty.setName("Тест Факультет");
+        faculty.setColor("синий");
+
+        ResponseEntity<String> response = restTemplate.postForEntity(
+                BASE_URL + port + "/faculty",
+                faculty,
+                String.class
+        );
+        assertNotNull(response.getBody());
+    }
+
+    @Test
+    void editFacultyTest() {
+        Faculty faculty = new Faculty();
+        faculty.setId(1L);
+        faculty.setName("Обновленный Факультет");
+        faculty.setColor("красный");
+
+        restTemplate.put(BASE_URL + port + "/faculty", faculty);
+        assertNotNull(restTemplate);
+    }
+
+    @Test
+    void deleteFacultyTest() {
+        restTemplate.delete(BASE_URL + port + "/faculty/1");
+        assertNotNull(restTemplate);
+    }
+
+    @Test
+    void getAllFacultiesTest() {
+        ResponseEntity<String> response = restTemplate.getForEntity(
+                BASE_URL + port + "/faculty",
+                String.class
+        );
+        assertNotNull(response.getBody());
+    }
+
+    @Test
+    void findFacultiesByColorTest() {
+        ResponseEntity<String> response = restTemplate.getForEntity(
+                BASE_URL + port + "/faculty?color=синий",
+                String.class
+        );
+        assertNotNull(response.getBody());
+    }
+
+    @Test
+    void searchFacultiesTest() {
+        ResponseEntity<String> response = restTemplate.getForEntity(
+                BASE_URL + port + "/faculty/search?search=тест",
+                String.class
+        );
+        assertNotNull(response.getBody());
+    }
+
+    @Test
+    void getFacultyStudentsTest() {
+        ResponseEntity<String> response = restTemplate.getForEntity(
+                BASE_URL + port + "/faculty/1/students",
+                String.class
+        );
+        assertNotNull(response.getBody());
+    }
+}
--- src/test/java/ru/hogwarts/school/webMvcTest/FacultyControllerWebMvcTest.java
+++ src/test/java/ru/hogwarts/school/webMvcTest/FacultyControllerWebMvcTest.java
@@ -1,4 +1,4 @@
-package ru.hogwarts.school;
+package ru.hogwarts.school.webMvcTest;
 
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
@@ -21,7 +21,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(FacultyController.class)
-class FacultyControllerWebMvcTest {
+public class FacultyControllerWebMvcTest {
 
     @Autowired
     private MockMvc mockMvc;
@@ -70,7 +70,7 @@ class FacultyControllerWebMvcTest {
     }
 
     @Test
-    void tdeleteFacultyTest() throws Exception {
+    void deleteFacultyTest() throws Exception {
         mockMvc.perform(delete("/faculty/1"))
                 .andExpect(status().isNoContent());
     }
@@ -128,118 +128,5 @@ class FacultyControllerWebMvcTest {
         mockMvc.perform(get("/faculty/1/students"))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.length()").value(0));
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.test.web.server.LocalServerPort;
-import org.springframework.http.ResponseEntity;
-import ru.hogwarts.school.controller.FacultyController;
-import ru.hogwarts.school.model.Faculty;
-
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class FacultyControllerRestTemplateTest {
-
-    private static final String BASE_URL = "http://localhost:";
-
-    @LocalServerPort
-    private int port;
-
-    @Autowired
-    private TestRestTemplate restTemplate;
-
-    @Autowired
-    private FacultyController facultyController;
-
-    @Test
-    void contextLoads() {
-        assertNotNull(facultyController);
-    }
-
-    @Test
-    void getFacultyTest() {
-        ResponseEntity<Faculty> response = restTemplate.getForEntity(
-                BASE_URL + port + "/faculty/1",
-                Faculty.class
-        );
-        assertNotNull(response.getBody());
-    }
-
-    @Test
-    void getFacultyNotFoundTest() {
-        ResponseEntity<Faculty> response = restTemplate.getForEntity(
-                BASE_URL + port + "/faculty/999",
-                Faculty.class
-        );
-        assertNotEquals(200, response.getStatusCodeValue());
-    }
-
-    @Test
-    void createFacultyTest() {
-        Faculty faculty = new Faculty();
-        faculty.setName("Тест Факультет");
-        faculty.setColor("синий");
-
-        ResponseEntity<String> response = restTemplate.postForEntity(
-                BASE_URL + port + "/faculty",
-                faculty,
-                String.class
-        );
-        assertNotNull(response.getBody());
-    }
-
-    @Test
-    void editFacultyTest() {
-        Faculty faculty = new Faculty();
-        faculty.setId(1L);
-        faculty.setName("Обновленный Факультет");
-        faculty.setColor("красный");
-
-        restTemplate.put(BASE_URL + port + "/faculty", faculty);
-        assertNotNull(restTemplate);
-    }
-
-    @Test
-    void deleteFacultyTest() {
-        restTemplate.delete(BASE_URL + port + "/faculty/1");
-        assertNotNull(restTemplate);
-    }
-
-    @Test
-    void getAllFacultiesTest() {
-        ResponseEntity<String> response = restTemplate.getForEntity(
-                BASE_URL + port + "/faculty",
-                String.class
-        );
-        assertNotNull(response.getBody());
-    }
-
-    @Test
-    void findFacultiesByColorTest() {
-        ResponseEntity<String> response = restTemplate.getForEntity(
-                BASE_URL + port + "/faculty?color=синий",
-                String.class
-        );
-        assertNotNull(response.getBody());
-    }
-
-    @Test
-    void searchFacultiesTest() {
-        ResponseEntity<String> response = restTemplate.getForEntity(
-                BASE_URL + port + "/faculty/search?search=тест",
-                String.class
-        );
-        assertNotNull(response.getBody());
-    }
-
-    @Test
-    void getFacultyStudentsTest() {
-        ResponseEntity<String> response = restTemplate.getForEntity(
-                BASE_URL + port + "/faculty/1/students",
-                String.class
-        );
-        assertNotNull(response.getBody());
     }
 }
